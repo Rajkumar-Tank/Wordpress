b74b69e3243fabf621af9528c49604949317710f
Rajkumar-Tank <rajkumar.tank.7@gmail.com>
2025-05-17 20:42:01 +0530

    add featured image in plugin

--- wp-content/plugins/custom-product-manager/includes/cpm-shortcodes.php
+++ wp-content/plugins/custom-product-manager/includes/cpm-shortcodes.php
@@ -5,12 +5,20 @@ function cpm_product_list_shortcode($atts) {
         'posts_per_page' => 10,
     ]);
 
-    $output = '<ul class="cpm-product-list">';
+    $output = '<ul class="cpm-product-list" style="list-style: none; padding: 0;">';
+
     while ($products->have_posts()) {
         $products->the_post();
         $price = get_post_meta(get_the_ID(), '_cpm_price', true);
-        $output .= '<li><strong>' . get_the_title() . '</strong> - ₹' . esc_html($price) . '</li>';
+        $thumbnail = get_the_post_thumbnail(get_the_ID(), 'thumbnail', ['style' => 'max-width:100px; height:auto; display:block;']);
+
+        $output .= '<li style="margin-bottom: 20px;">';
+        $output .= $thumbnail;
+        $output .= '<strong>' . get_the_title() . '</strong><br>';
+        $output .= 'Price: ₹' . esc_html($price);
+        $output .= '</li>';
     }
+
     wp_reset_postdata();
     $output .= '</ul>';
 
